e870d689bae72567e20dfd6d0c46354a153be617
williamnerypereira <william.nery.pereira@gmail.com>
2021-10-05 12:25:21 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
-public class Product implements Serializable{
+public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
